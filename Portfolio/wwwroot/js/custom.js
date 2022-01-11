e6a3b53d861c999d0d9a1001dada14aaf0ce129e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-11 15:21:19 +0100

    add custom.js

---
+++ Portfolio/wwwroot/js/custom.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+/*-------------------------------------------------------------------------------
+  PRE LOADER
+-------------------------------------------------------------------------------*/
+
+$(window).on('load', function () {
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
