30c35e68ab641cebac56a8c090f8da79fab255c4
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-06 18:58:21 +0100

    implement ContactModel

--- Portfolio/Pages/Contact.cshtml.cs
+++ Portfolio/Pages/Contact.cshtml.cs
@@ -1,10 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Portfolio.Services;
 
 namespace Portfolio.Pages
 {
+    [ValidateAntiForgeryToken]
     public class ContactModel : PageModel
     {
+        private readonly IPortfolioEmailSender _emailSender;
+        public ContactModel(IPortfolioEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [BindProperty] public InputModel Input { get; set; } = new();
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(80, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+            public string Name { get; set; } = default!;
+
+            [Required]
+            [EmailAddress]
+            [StringLength(50, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 5)]
+            public string Email { get; set; } = default!;
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+            public string Subject { get; set; } = default!;
+
+            [Required]
+            [StringLength(500, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 10)]
+            public string Message { get; set; } = default!;
+        }
+
         public void OnGet()
         {
         }
