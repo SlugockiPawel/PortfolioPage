f7a597a6ed1254b6b3cc1608d1f2adc656b5ce40
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-06 18:57:54 +0100

    implement Email Service

---
+++ Portfolio/Services/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Portfolio.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace Portfolio.Services
+{
+    public class EmailService : IPortfolioEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage
+            {
+                Sender = MailboxAddress.Parse(_emailSettings.Email)
+            };
+
+            email.To.Add(MailboxAddress.Parse(_emailSettings.Email));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = $"<b>{name}</b> has sent you an email and can be reached at: <b>{emailFrom}</b><br/><br/>{htmlMessage}";
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_emailSettings.Email, _emailSettings.Password);
+
+            await smtp.SendAsync(email);
+
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
---
+++ Portfolio/Services/IPortfolioEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Portfolio.Services
+{
+    public interface IPortfolioEmailSender
+    {
+        Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage);
+    }
+}
