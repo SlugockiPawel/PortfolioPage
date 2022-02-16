4943c86df0c3e9f26dabdb2f4688516940374469
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-16 21:23:11 +0100

    implement ProjectModel

---
+++ Portfolio/ViewModels/ProjectModel.cs
@@ -0,0 +1,18 @@
+﻿using Portfolio.Enums;
+
+namespace Portfolio.ViewModels
+{
+    public class ProjectModel
+    {
+        public int Id { get; init; }
+        public bool MainProject { get; set; } = true;
+        public string Title { get; init; } = default!;
+        public string Abstract { get; init; } = default!;
+        public string Description { get; init; } = default!;
+        public List<string>? Images { get; init; }
+        public List<Technology>? Technologies { get; init; }
+        public DateOnly Date { get; init; } = DateOnly.FromDateTime(DateTime.Today);
+        public string ProjectUrl { get; init; } = default!;
+        public string GithubUrl { get; init; } = default!;
+    }
+}
