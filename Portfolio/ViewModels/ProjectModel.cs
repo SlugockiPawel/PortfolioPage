a0dcbb20bc6f91ee7cf214684b082559bea7c040
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-18 13:44:44 +0100

    implement ProjectModel

--- Portfolio/ViewModels/ProjectModel.cs
+++ Portfolio/ViewModels/ProjectModel.cs
@@ -5,11 +5,13 @@ namespace Portfolio.ViewModels
     public class ProjectModel
     {
         public int Id { get; init; }
-        public bool MainProject { get; set; } = true;
+        public bool IsMainProject { get; init; } = true;
+        public bool IsLeadProject { get; init; } = false;
         public string Title { get; init; } = default!;
         public string Abstract { get; init; } = default!;
         public string Description { get; init; } = default!;
-        public List<string>? Images { get; init; }
+        public string CardImage { get; init; } = default!;
+        public List<string>? CarouselImages { get; init; }
         public List<Technology>? Technologies { get; init; }
         public DateOnly Date { get; init; } = DateOnly.FromDateTime(DateTime.Today);
         public string ProjectUrl { get; init; } = default!;
