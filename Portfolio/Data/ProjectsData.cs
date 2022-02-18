779a41088911063bae375c1ad56e46c8527e2093
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-18 13:45:47 +0100

    add Projects to ProjectsData static class
    it will allow me to use Projects list in various places accross app

---
+++ Portfolio/Data/ProjectsData.cs
@@ -0,0 +1,57 @@
+﻿using Portfolio.Enums;
+using Portfolio.ViewModels;
+
+namespace Portfolio.Data
+{
+    public static class ProjectsData
+    {
+        public static List<ProjectModel> ProjectsList { get;  } = new();
+
+        static ProjectsData()
+        {
+            ProjectsList.AddRange(new List<ProjectModel>
+            {
+                new ProjectModel()
+                {
+                    Id = 1,
+                    Title = "Bug Tracker",
+                    Abstract = "This is some abstract for now. Update it later",
+                    Description = "This is broader description. Update it later.",
+                    Date = new DateOnly(2022, 02, 15),
+                    GithubUrl = "https://gitHubUrl",
+                    ProjectUrl = "https://www.slugocki.dev",
+                    CardImage = "images/projects/project-4.jpg",
+                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/project-4.jpg" },
+                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+                    IsLeadProject = true,
+                },
+                new ProjectModel()
+                {
+                    Id = 2,
+                    Title = "Blog Project",
+                    Abstract = "This is some abstract for now for The Blog. Update it later",
+                    Description = "This is broader description. Update it later.",
+                    Date = new DateOnly(2022, 02, 13),
+                    GithubUrl = "https://gitHubUrl",
+                    ProjectUrl = "https://www.slugocki.dev",
+                    CardImage = "images/projects/project-4.jpg",
+                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/project-4.jpg" },
+                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+                },
+                new ProjectModel()
+                {
+                    Id = 2,
+                    Title = "The Movie Store",
+                    Abstract = "This is some abstract for now for The Movie Store. Update it later",
+                    Description = "This is broader description. Update it later.",
+                    Date = new DateOnly(2022, 02, 13),
+                    GithubUrl = "https://gitHubUrl",
+                    ProjectUrl = "https://www.slugocki.dev",
+                    CardImage = "images/projects/project-4.jpg",
+                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/project-4.jpg" },
+                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+                },
+            });
+        }
+    }
+}
