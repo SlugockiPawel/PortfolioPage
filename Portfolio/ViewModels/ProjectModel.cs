b826b3e283c024d53d6438cfc882ffcb77abbefe
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-18 19:47:44 +0100

    update ProjectModel

--- Portfolio/ViewModels/ProjectModel.cs
+++ Portfolio/ViewModels/ProjectModel.cs
@@ -11,8 +11,8 @@ namespace Portfolio.ViewModels
         public string Abstract { get; init; } = default!;
         public string Description { get; init; } = default!;
         public string CardImage { get; init; } = default!;
-        public List<string>? CarouselImages { get; init; }
-        public List<Technology>? Technologies { get; init; }
+        public List<string> CarouselImages { get; init; } = new();
+        public List<Technology> Technologies { get; init; } = new();
         public DateOnly Date { get; init; } = DateOnly.FromDateTime(DateTime.Today);
         public string ProjectUrl { get; init; } = default!;
         public string GithubUrl { get; init; } = default!;
