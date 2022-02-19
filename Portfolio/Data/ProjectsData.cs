626011609c9566f511cb8602f42e20102830f959
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-19 14:18:11 +0100

    add miniproject example

--- Portfolio/Data/ProjectsData.cs
+++ Portfolio/Data/ProjectsData.cs
@@ -1,73 +1,86 @@
 ﻿using Portfolio.Enums;
 using Portfolio.ViewModels;
 
-namespace Portfolio.Data
+namespace Portfolio.Data;
+
+public static class ProjectsData
 {
-    public static class ProjectsData
+    public static List<ProjectModel> ProjectsList { get;  } = new();
+
+    static ProjectsData()
     {
-        public static List<ProjectModel> ProjectsList { get;  } = new();
-
-        static ProjectsData()
+        ProjectsList.AddRange(new List<ProjectModel>
         {
-            ProjectsList.AddRange(new List<ProjectModel>
+            new ProjectModel()
             {
-                new ProjectModel()
-                {
-                    Id = 1,
-                    Title = "Bug Tracker",
-                    Abstract = "This is some abstract for now. Update it later",
-                    Description = "This is broader description. Update it later.",
-                    Date = new DateOnly(2022, 02, 15),
-                    GithubUrl = "https://gitHubUrl",
-                    ProjectUrl = "https://www.slugocki.dev",
-                    CardImage = "images/projects/project-4.jpg",
-                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/item-6.jpg" },
-                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
-                    IsLeadProject = true,
-                },
-                new ProjectModel()
-                {
-                    Id = 2,
-                    Title = "Blog Project",
-                    Abstract = "This is some abstract for now for The Blog. Update it later",
-                    Description = "This is broader description. Update it later.",
-                    Date = new DateOnly(2022, 02, 13),
-                    GithubUrl = "https://gitHubUrl",
-                    ProjectUrl = "https://www.slugocki.dev",
-                    CardImage = "images/projects/project-4.jpg",
-                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/project-4.jpg" },
-                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
-                },
-                new ProjectModel()
-                {
-                    Id = 3,
-                    Title = "The Movie Store",
-                    Abstract = "This is some abstract for now for The Movie Store. Update it later",
-                    Description = "This is broader description. Update it later.",
-                    Date = new DateOnly(2022, 02, 13),
-                    GithubUrl = "https://gitHubUrl",
-                    ProjectUrl = "https://www.slugocki.dev",
-                    CardImage = "images/projects/project-4.jpg",
-                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/item-4.jpg",  "images/projects/item-6.jpg" },
-                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
-                },
+                Id = 1,
+                Title = "Bug Tracker",
+                Abstract = "This is some abstract for now. Update it later",
+                Description = "This is broader description. Update it later.",
+                Date = new DateOnly(2022, 02, 15),
+                GithubUrl = "https://gitHubUrl",
+                ProjectUrl = "https://www.slugocki.dev",
+                CardImage = "images/projects/project-4.jpg",
+                CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/item-6.jpg" },
+                Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+                IsLeadProject = true,
+            },
+            new ProjectModel()
+            {
+                Id = 2,
+                Title = "Blog Project",
+                Abstract = "This is some abstract for now for The Blog. Update it later",
+                Description = "This is broader description. Update it later.",
+                Date = new DateOnly(2022, 02, 13),
+                GithubUrl = "https://gitHubUrl",
+                ProjectUrl = "https://www.slugocki.dev",
+                CardImage = "images/projects/project-4.jpg",
+                CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/project-4.jpg" },
+                Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+            },
+            new ProjectModel()
+            {
+                Id = 3,
+                Title = "The Movie Store",
+                Abstract = "This is some abstract for now for The Movie Store. Update it later",
+                Description = "This is broader description. Update it later.",
+                Date = new DateOnly(2022, 02, 13),
+                GithubUrl = "https://gitHubUrl",
+                ProjectUrl = "https://www.slugocki.dev",
+                CardImage = "images/projects/project-4.jpg",
+                CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/item-4.jpg",  "images/projects/item-6.jpg" },
+                Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+            },
 
-                // Mini projects
-                new ProjectModel()
-                {
-                    Id = 4,
-                    IsMainProject = false,
-                    Title = "The Loan Unicorn",
-                    Abstract = "This is some abstract for now for The Loan Unicorn. Update it later",
-                    Description = "This is broader description for Loan Unicorn. Update it later.",
-                    Date = new DateOnly(2022, 02, 13),
-                    GithubUrl = "https://gitHubUrl",
-                    ProjectUrl = "https://www.slugocki.dev",
-                    CardImage = "images/projects/item-4.jpg",
-                    CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/item-4.jpg",  "images/projects/item-6.jpg" },
-                    Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
-                },
-            });
-        }
+            // Mini projects
+            new ProjectModel()
+            {
+                Id = 4,
+                IsMainProject = false,
+                Title = "The Loan Unicorn",
+                Abstract = "This is some abstract for now for The Loan Unicorn. Update it later",
+                Description = "This is broader description for Loan Unicorn. Update it later.",
+                Date = new DateOnly(2022, 02, 13),
+                GithubUrl = "https://gitHubUrl",
+                ProjectUrl = "https://www.slugocki.dev",
+                CardImage = "images/projects/item-4.jpg",
+                CarouselImages = new List<string>() { "images/projects/project-4.jpg", "images/projects/item-4.jpg",  "images/projects/item-6.jpg" },
+                Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+            },
+            new ProjectModel()
+            {
+                Id = 5,
+                IsMainProject = false,
+                Title = "FizzBuzz",
+                Abstract = "This is some abstract for now for FizzBuzz. Update it later",
+                Description = "This is broader description for FizzBuzz. Update it later.",
+                Date = new DateOnly(2022, 02, 13),
+                GithubUrl = "https://fizz.com",
+                ProjectUrl = "https://www.slugocki.dev",
+                CardImage = "images/projects/item-6.jpg",
+                CarouselImages = new List<string>() { "images/projects/project-4.jpg",  "images/projects/item-6.jpg" },
+                Technologies = new List<Technology>() { Technology.Bootstrap5, Technology.CSS3 },
+            },
+        });
     }
 }
