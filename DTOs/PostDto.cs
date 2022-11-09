6ca0e7b125a947051ace83fd2bc1bd9db715f80d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-11-09 15:55:51 +0100

    add slug to PostDto

--- DTOs/PostDto.cs
+++ DTOs/PostDto.cs
@@ -5,6 +5,7 @@ namespace PortfolioPage.DTOs;
 public class PostDto
 {
     public string Title { get; set; }
+    public string Slug { get; set; }
     public string Abstract { get; set; }
     public byte[] ImageData { get; set; }
     public string ContentType { get; set; }
